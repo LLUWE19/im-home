5ed6f436c4db9d77953c33c27cf50ee550c53cad
Your Name <you@example.com>
2019-09-03 16:29:20 +0100

    added color and brightness

--- action-leonard_home.py
+++ action-leonard_home.py
@@ -16,8 +16,14 @@ MQTT_ADDR = "{}:{}".format(MQTT_IP_ADDR, str(MQTT_PORT))
 
 INTENT_IM_HOME = "LLUWE19:user_arrives_home"
 INTENT_ANSWER = "LLUWE19:user_gives_answer"
+INTENT_PERCENTAGE = "LLUWE19:user_gives_percentage"
+INTENT_COLOR = "LLUWE19:user_gives_color"
 
 last_question = None
+light_on = False
+light_color = None
+light_brightness = None
+tv_on = False
 SessionStates = {}
 
 
@@ -47,50 +53,90 @@ def user_arrives_home(hermes, intent_message):
 
 
 def user_gives_answer(hermes, intent_message):
+    global light_brightness
+    global light_color
+    global light_on
+    global tv_on
+    global last_question
+
     print("User is giving an answer...")
     print("Reading the config file")
     conf = read_configuration_file(CONFIGURATION_INI)
     autho = conf['secret']['http_api_password']
-    print("The access token is: " + autho)
     header = {
         'Authorization': autho,
         "Content-Type": "application/json",
-
     }
     session_id = intent_message.session_id
 
     answer = None
+
     if intent_message.slots.answer:
         answer = intent_message.slots.answer.first().value
         print("The user answered: " + answer)
 
-    global last_question
+    if intent_message.slots.color:
+        color = intent_message.slots.color.first().value
+
+    if intent_message.slots.percentage:
+        percentage = intent_message.slots.percentage.first().value
+
     if last_question == "welcome home... would you like the lights on":
         if answer == "yes":
-            print("!!!!!!!Turning on the light!!!!!!!!!!!")
-            url = 'http://192.168.0.136:8123/api/services/light/turn_on'
-            body = {
-                "entity_id": "light.tall_lamp"
-            }
-            json_body = json.dumps(body)
-            request = rq.post(url, data=json_body, headers=header)
+            light_on = True
+            sentence = "okay... what color do you want the light"
         else:
-            print("Leaving the light off")
-        sentence = "okay... do you want the tee vee on"
+            light_on = False
+            sentence = "okay... did you want the tee vee on"
         last_question = sentence
         hermes.publish_continue_session(session_id, sentence, [INTENT_ANSWER])
-    elif last_question == "okay... do you want the tee vee on":
-        if answer == "yes":
-            print("Turning on the tv")
-            url = 'http://192.168.0.136:8123/api/services/switch/turn_on'
-            body = {
-                "entity_id": "switch.living_room_tv"
-            }
-            json_body = json.dumps(body)
-            request = rq.post(url, data=json_body, headers=header)
+
+    if last_question == "okay... what color do you want the light" and light_on is True:
+        if color:
+            light_color = color
+            print("User responded with color")
         else:
+            light_color = "white"
+        sentence = "okay... how bright do you want the light"
+        last_question = sentence
+        hermes.publish_continue_session(session_id, sentence, [INTENT_ANSWER])
+
+    if last_question == "okay... how bright do you want the light":
+        print("User responded with brightness")
+        if percentage:
+            light_brightness = percentage
+        else:
+            light_brightness = 100
+        sentence = "okay... did you want the tee vee on"
+        last_question = sentence
+        hermes.publish_continue_session(session_id, sentence, [INTENT_ANSWER])
+
+    if last_question == "okay... did you want the tee vee on":
+        if answer == "yes":
+            tv_on = True
+        else:
+            tv_on = False
             print("Leaving the tv off")
         sentence = "okay... welcome home"
+
+        print("Turning on the light")
+        url = 'http://192.168.0.136:8123/api/services/light/turn_on'
+        body = {
+            "entity_id": "light.tall_lamp",
+            "color": light_color,
+            "brightness": light_brightness
+        }
+        json_body = json.dumps(body)
+        request = rq.post(url, data=json_body, headers=header)
+
+        print("Turning on the tv")
+        url = 'http://192.168.0.136:8123/api/services/switch/turn_on'
+        body = {
+            "entity_id": "switch.living_room_tv"
+        }
+        json_body = json.dumps(body)
+        request = rq.post(url, data=json_body, headers=header)
+
         last_question = sentence
         hermes.publish_end_session(session_id, sentence)
 
@@ -99,4 +145,15 @@ with Hermes(MQTT_ADDR) as h:
 
     h.subscribe_intent(INTENT_IM_HOME, user_arrives_home) \
         .subscribe_intent(INTENT_ANSWER, user_gives_answer) \
+        .subscribe_intent(INTENT_COLOR, user_gives_answer()) \
+        .subscribe_intent(INTENT_PERCENTAGE, user_gives_answer()) \
         .start()
+
+
+
+
+
+
+
+
+
